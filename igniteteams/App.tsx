f29de93df29024de7ef58633ce8fee2785a02582
Davi Matheus <davifox@hotmail.com>
2023-09-28 18:40:15 -0300

    Add fonts

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -1,11 +1,16 @@
 import { Groups } from "@screens/Groups";
 import { ThemeProvider } from "styled-components/native";
 import theme from "./src/theme";
+import { useFonts, Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { ActivityIndicator } from "react-native";
 
 export default function App() {
+   const [fontsLoader] = useFonts({Roboto_400Regular, Roboto_700Bold});
+
+
   return (
     <ThemeProvider theme={theme}>
-    <Groups/>
+    {fontsLoader ? <Groups/> : <ActivityIndicator/>}                              /* carregamento da font condicional */
     </ThemeProvider>
   );
 }
