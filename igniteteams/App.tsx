f7a7901b3fa0de440c5713916ba2f356b51a9085
Davi Matheus <davifox@hotmail.com>
2023-09-29 18:58:26 -0300

    Add GroupCard

--- igniteteams/App.tsx
+++ igniteteams/App.tsx
@@ -15,7 +15,7 @@ export default function App() {
       backgroundColor="transparent"
       translucent
       />
-      <Groups/>      
+      {fontsLoader ? <Groups/> : <ActivityIndicator/>}      
     </ThemeProvider>
   );
 }
