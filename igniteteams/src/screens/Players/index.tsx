8bee9fb9014631e98d770a8c429e6af00c6da680
Davi Matheus <davifox@hotmail.com>
2023-10-10 17:47:06 -0300

    add tudo q faltou

--- igniteteams/src/screens/Players/index.tsx
+++ igniteteams/src/screens/Players/index.tsx
@@ -1,8 +1,10 @@
-import  { useState } from 'react';
+import { useState } from 'react';
 import { FlatList } from "react-native";
 import { Input } from "@components/Input";
 import { Filter } from "@components/Filter";
 import { Header } from "@components/Header";
+import { Button } from '@components/Button';
+import { ListEmpty } from '@components/ListEmpty';
 import { Highlight } from "@components/Highlight";
 import { ButtonIcon } from "@components/ButtonIcon";
 import { PlayerCard } from '@components/PlayerCard';
@@ -11,7 +13,7 @@ import { Container, Form, HeaderList, NumberOfPlayers } from "./style";
 
 export function Players(){
     const [team, setTeam] = useState('Time A');
-    const [players, setPlayers] = useState(["Davi", "Ribeiro"]);
+    const [players, setPlayers] = useState([]);
 
     return(
         <Container>
@@ -57,6 +59,19 @@ export function Players(){
                         onRemove={() => {}}
                         />
                 )}
+                ListEmptyComponent={() => (
+                    <ListEmpty 
+                        message="N'ao ha pessoas nesse time" />
+                )}
+                showsVerticalScrollIndicator={false}
+                contentContainerStyle={[
+                    {paddingBottom: 100},
+                    players.length === 0 && { flex: 1 },
+                ]}
+            />
+            <Button
+                title='Remover Turma'
+                type = "SECONDARY"
             />
         </Container>
     );
