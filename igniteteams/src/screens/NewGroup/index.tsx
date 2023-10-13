f816e74bb66435f732ad8191726b57f2e8838476
Davi Matheus <davifox@hotmail.com>
2023-10-13 01:57:36 -0300

    add excecoes

--- igniteteams/src/screens/NewGroup/index.tsx
+++ igniteteams/src/screens/NewGroup/index.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
+import { Alert } from 'react-native';
 import { Input } from "@components/Input";
+import { AppError } from "@utils/AppError";
 import { Header } from "@components/Header";
 import { Button } from "@components/Button";
 import { Highlight } from "@components/Highlight";
@@ -16,10 +18,20 @@ export function NewGroup(){
 
     async function handleNew(){
         try{
+            if(group.trim().length === 0){
+                return Alert.alert('Novo Grupo','Informe o nome da turma.');
+             }
             await groupCreate(group);
             navigation.navigate('players', {group});
+
         }catch(error){
-          console.log(error);
+            if(error instanceof AppError){
+                Alert.alert('Novo Grupo',error.message);
+            }else{
+                Alert.alert('Novo Grupo','Nao foi possivel criar um novo grupo.');
+                console.log(error);
+
+            }
         }
       }
     return(
